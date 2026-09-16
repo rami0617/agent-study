246cc2df571a92f605ad0d4ac33741e83fbfaa9b
최아람 <aarmakrr@hanmail.net>
2026-09-16 17:51:40 +0900

    mcp 서버 만들기

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,34 @@
-import { query } from "@anthropic-ai/claude-agent-sdk";
+import { query, tool, createSdkMcpServer } from "@anthropic-ai/claude-agent-sdk";
+
+const getTimeTool = tool(
+  'getTime',
+ '시간을 반환하는 mcp이다.',
+  {}, 
+  async () => {
+    const hour = new Date().getHours();
+    const minutes =  new Date().getMinutes()
+    return {
+      content: [{ type: "text", text: `${hour}:${minutes}` }],
+    };
+  },
+);
+
+const myServer = createSdkMcpServer({
+  name: 'server',
+  tools: [getTimeTool],
+});
 
 async function main() {
-  const result = query({
-    prompt: "지금 몇 번째 메시지를 보내고 있는지 말해줘",
+  for await (const message of query({
+    prompt: "지금 몇 시야?",
     options: {
       cwd: process.cwd(),
       systemPrompt: "당신은 친절한 어시스턴트입니다.",
-      tools: [],
-    }
-  });
-
-  for await (const message of result) {
+      mcpServers: { 'server': myServer },
+      tools: [ 'mcp__server__getTime'],
+      allowedTools: [ 'mcp__server__getTime' ]
+    },
+  })) {
     console.log(JSON.stringify(message, null, 2));
   }
 }
